2e109abe6ccdfdc7110a662f1bd8832e00b23507
ingridmicaa <ingridmicaela03@gmail.com>
2025-12-18 11:02:31 -0500

    Ejercicios

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -14,7 +14,45 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}};
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] totalAsistencias = new int[estudiantes.length];
+
+        for (int i = 0; i < asistencia.length; i++) {
+            int indice = 0;
+
+            for (int col = 0; col < asistencia[i].length; col++) {
+
+                if (asistencia[i][col]) {
+                    indice++;
+                }
+
+            }
+            
+            totalAsistencias[i]= indice;
+            
+        }
+        
+        for (int i = 0; i < estudiantes.length; i++) { 
+            System.out.printf("Estudiante:%s\n Asistencias: %d\n ", 
+                    estudiantes[i],
+                    totalAsistencias[i]);
+            if (totalAsistencias[i] == 5) { 
+                
+                System.out.println("Asistencia completa");
+                
+            }else { 
+                
+                System.out.println("No tiene asistencia completa");
+            }
+        } 
     }
-    
+
 }
--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,34 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}};
+        int menores = 0;
+        int[] adultosF = new int[edades.length];
+
+        for (int f = 0; f < edades.length; f++) {
+            int adultos = 0;
+            for (int c = 0; f < edades[f].length; c++) {
+                if (edades[f][c] <= 18) {
+                    menores++;
+                    
+                }else{
+                    adultos++;
+                }
+                adultosF[f]= adultos;
+            }
+            System.out.printf("Total de menores de edad:%s\n ", menores);
+            
+            for (int i = 0; i < adultosF.length; i++) {
+                
+                System.out.println("Familia " + (i + 1) + ": " + adultosF[i] + 
+                        " adultos");
+            }
+        }
+    }
 }
